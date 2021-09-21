4ec5a9ffe05eae614d091dc925dbb6a9f2e38c30
Shelsy Elaine <elainesbarrera@gmail.com>
2021-09-21 13:12:58 -0700

    ADDED: pull code, login page, proxy link, concurrently

---
+++ client/src/componets/pages/Login.js
@@ -0,0 +1,10 @@
+import React, { useEffect } from "react";
+
+const Login = () => {
+  useEffect(() => {
+    console.log("hi");
+  }, []);
+  return <div>Hello Login</div>;
+};
+
+export default Login;
