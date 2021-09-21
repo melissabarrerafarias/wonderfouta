98199f9bbb89684818ac5d510027264a59c7fa8e
Shelsy Elaine <elainesbarrera@gmail.com>
2021-09-21 13:55:36 -0700

    ADDED: global context user and token, set state, usehistory for protected pages

--- client/src/componets/pages/Login.js
+++ client/src/componets/pages/Login.js
@@ -1,11 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import UserContext from "../../context/UserContext";
 
 const Login = () => {
+  const [data, setData] = useState({ user: undefined, token: undefined });
   const checkToken = async () => {
-    let token = await localStorage.getItem("auth-token");
+    try {
+      let token = await localStorage.getItem("auth-token");
 
-    if (token === null) {
-      localStorage.setItem("auth-token", "");
+      if (token === null) {
+        localStorage.setItem("auth-token", "");
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
   useEffect(() => {
@@ -15,3 +21,15 @@ const Login = () => {
 };
 
 export default Login;
+
+
+// use this code in every protected page
+// import { useHistory } from "react-router-dom";
+// const history = useHistory();
+// const { data } = useContext(UserContext);
+
+// useEffect(() => {
+//   if (!data.user) {
+//     history.push("/login");
+//   }
+// }, [data.user, history]);
