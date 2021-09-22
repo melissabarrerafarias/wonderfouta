cb84d5476efb9ed4dbc433e96ba2c6d4e8e0cb47
unknown <melissikiss@gmail.com>
2021-09-22 11:33:23 -0700

    added ability to change product depending on clicked towel, hardcoded values of initial image

--- client/src/components/towelCollection.js/TowelCollection.js
+++ client/src/components/towelCollection.js/TowelCollection.js
@@ -3,53 +3,53 @@ import { useParams } from 'react-router-dom';
 import { commerce } from '../../lib/commerce';
 
 function TowelCollection({ onAddToCart }) {
-    const { id: productId } = useParams(); // id = productId
+    const { id: productId } = useParams(); // id = productId (from parameters)
 
-    const [singleProduct, setSingleProduct] = useState([]);
+    const [collection, setCollection] = useState([]); // getting all data
 
-    const [selectedProduct, setSelectedProduct] = useState({ id: undefined, img: 'https://cdn.chec.io/merchants/33461/assets/D5TD6zDyjFlWX5hS|Arthur Fouta NutmegAndBlack.jpg' }); 
+    const [selectedProduct, setSelectedProduct] = useState({
+        img: "https://cdn.chec.io/merchants/33461/assets/D5TD6zDyjFlWX5hS%7CArthur%20Fouta%20NutmegAndBlack.jpg", 
+        id: "vrnt_L1vOoZPbQplRa8", 
+        description: "Arthur Fouta Nutmeg / black stripes Dimensions: 40x80 inches Content: 100% cotton Weight: 0.93 oz Our chic and modern personal size fouta's  are the perfect addition for your busy life. The fouta with all of its many uses is not only eco-friendly,  super absorbent,  and lightweight  as a towel, but when heading to the beach(it does not retain sand), poolside, as a lightweight traveling blanket,  a quick and stylish shawl , and another FUN FACT:  5 fouta's can fit in the same space as 1 terrycloth towel", 
+        price: "$32.00"
+    }); // setting initial image on page load but changing onClick to selected image
 
     const retrieveProductVariants = async () => {
-        const productVariants = await commerce.products.getVariants(productId);
-        console.log(productVariants);
-      }
+        const { data } = await commerce.products.getVariants(productId);
+        setCollection(data);
+    } // setting state to specific collection variants 
 
-    const retrieveSingleProduct = async () => {
-        const singleProduct = await commerce.products.retrieve(productId);
-        setSingleProduct(singleProduct)
-    }
-
-    // const displaySingleProduct = async (e) => {
-    //     setSelectedProduct({ img: e.target.src })
-    //     console.log(e.target)
-    // }
+    const displaySingleProduct = async (e) => {
+        setSelectedProduct(
+            { img: e.target.src, id: e.target.id, description: e.target.dataset.description, price: e.target.dataset.price }
+        )
+    } // setting selected product state to selected product 
 
     useEffect(() => {
-        retrieveSingleProduct();
         retrieveProductVariants();
-    }, [])
+    }, []) // retrieving data on page load ^
+    console.log(collection)
 
-    // let assets = singleProduct.assets
-    // console.log(singleProduct)
     return (
-        <div> hi </div>
-        // <div>
-        //     <p>hi this is where a single towel collection is gonna go</p>
-        //     <div>
-        //         <img src={selectedProduct.img}></img>
-        //         <p>{singleProduct.name}</p>
-        //         <h3>{singleProduct.price?.formatted_with_symbol}</h3>
-        //         <p dangerouslySetInnerHTML={{ __html: singleProduct.description }}></p>
-        //         <button onClick={() => onAddToCart(selectedProduct.id, 1)}>Add to Cart</button>{/*functionality to add to cart */}
-        //     </div>
-        //     <div>
-        //         {assets?.map((variant) => (
-        //             <div onClick={(e) => displaySingleProduct(e)} key={variant.id} >
-        //                 <img src={variant.url} id={variant.id} />
-        //             </div>
-        //         ))}
-        //     </div>
-        // </div>
+        <div>
+            <div>
+                <img src={selectedProduct.img}></img>
+                <h3>{selectedProduct.price}</h3>
+                <p dangerouslySetInnerHTML={{ __html: selectedProduct.description }}></p>
+                <button onClick={() => onAddToCart(selectedProduct.id, 1)}>Add to Cart</button>{/*functionality to add to cart */}
+            </div>
+            <div>
+                {collection?.map((variant) => (
+                    <div onClick={(e) => displaySingleProduct(e)} key={variant.id} >
+                        <img src={variant.assets[0].url}
+                            id={variant.id}
+                            data-description={variant.description}
+                            data-price={variant.price.formatted_with_symbol}
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 };
 
