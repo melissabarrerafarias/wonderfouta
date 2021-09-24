a427a0af99725d60589c5afd4d0fcea02ac80856
unknown <melissikiss@gmail.com>
2021-09-24 12:15:08 -0700

    added stock alert in case of low in stock

--- client/src/components/towelCollection.js/TowelCollection.js
+++ client/src/components/towelCollection.js/TowelCollection.js
@@ -11,7 +11,7 @@ function TowelCollection({ onAddToCart }) {
 
     const [selectedProduct, setSelectedProduct] = useState({}); // changing onClick to selected image
 
-    const [quantity, setQuantity] = useState('1'); 
+    const [quantity, setQuantity] = useState('1');
 
     function updateDesiredQuantity(e) {
         setQuantity(e.target.value)
@@ -21,11 +21,11 @@ function TowelCollection({ onAddToCart }) {
         const product = await commerce.products.retrieve(productId);
         let names = product.variant_groups[0].options;
         setTowelNames(names);
-    } 
+    }
 
     const retrieveProductVariants = async () => { // setting state to specific collection variants 
         const { data } = await commerce.products.getVariants(productId);
-        console.log(data) // in data we find INVENTORY (can be used for later as 'stock')
+        // console.log(data) // in data we find INVENTORY (can be used for later as 'stock')
         setCollection(data);
         setSelectedProduct({ //setting initial image on page load
             img: data[1]?.assets[0].url,
@@ -37,25 +37,39 @@ function TowelCollection({ onAddToCart }) {
 
     const displaySingleProduct = async (e) => { // setting selected product state to selected product
         setSelectedProduct(
-            { img: e.target.src, id: e.target.id, description: e.target.dataset.description, price: e.target.dataset.price }
+            { img: e.target.src, id: e.target.id, description: e.target.dataset.description, price: e.target.dataset.price, stock: e.target.dataset.stock }
         )
-    } 
+    }
 
     useEffect(() => {
         retrieveProductVariants();
         retrieveName();
     }, []) // retrieving data on page load ^
 
-    // console.log(selectedProduct)
+    console.log(selectedProduct)
+
+    // const LowStock = () => {
+    //     if (selectedProduct.stock < 3) {
+    //         return (
+
+    //         )
+    //     }
+    // }
+
     return (
         <div>
             {/* selected towel */}
             <div>
                 <img src={selectedProduct.img}></img>
                 <h3>{selectedProduct.price}</h3>
+                {selectedProduct.stock < 3 &&
+                    <div>
+                        <p>Only {selectedProduct.stock} left in stock!</p>
+                    </div>
+                }
                 <p dangerouslySetInnerHTML={{ __html: selectedProduct.description }}></p>
-                <input placeholder = "Quantity" value={quantity} onChange={updateDesiredQuantity}></input>
-                <button type = "submit" onClick={() => { onAddToCart(productId, quantity, selectedProduct.id); setQuantity('1') } }>Add to Cart</button>{/*functionality to add to cart */}
+                <input placeholder="Quantity" value={quantity} onChange={updateDesiredQuantity}></input>
+                <button type="submit" onClick={() => { onAddToCart(productId, quantity, selectedProduct.id); setQuantity('1') }}>Add to Cart</button>{/*functionality to add to cart */}
             </div>
 
             {/* all towels styles */}
@@ -66,6 +80,7 @@ function TowelCollection({ onAddToCart }) {
                             id={variant?.id}
                             data-description={variant?.description}
                             data-price={variant.price?.formatted_with_symbol}
+                            data-stock={variant.inventory}
                         />
                         {towelNames[index]?.name} {/* shows towel name/color */}
                     </div>
