2d80b4193870d5b33c8d25aac1b9f5bf011821a0
Shelsy Elaine <elainesbarrera@gmail.com>
2021-09-30 14:25:38 -0700

    ADDE: checkout review order component

--- client/src/components/checkoutForm/Review.js
+++ client/src/components/checkoutForm/Review.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React from "react";
 
-const Review = ({checkoutToken}) => {
+const Review = ({ checkoutToken }) => {
+  console.log(checkoutToken);
+  return (
+    <>
+      <ul>
+        {checkoutToken.live.line_items.map((product) => (
+          <li key={product.name}>
+            <img src={product.media.source} style={{ maxWidth: "100px" }} />
+            Name: {product.name}
+            Color: {product.selected_options[0].option_name}
+            quantity: {product.quantity}
+            Total: {product.line_total.formatted_with_symbol}
+          </li>
+        ))}
+        <li>Subtotal: {checkoutToken.live.subtotal.formatted_with_symbol}</li>
+      </ul>
+    </>
+  );
+};
 
-    console.log(checkoutToken)
-    return (
-        <div>
-            <li>Hello</li>
-        </div>
-    )
-}
-
-export default Review
+export default Review;
